700b47b0dfc515fc954ccaed3d3214ac948fba79
Kimberly <work223305@gmail.com>
2024-10-23 15:28:43 +0800

    Change Database

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 
 export default function SiteHeader() {
-  const { loading, error, data } = useFetch('https://grounded-compassion-7a840a9fe7.strapiapp.com/api/categories') // 呼叫所有評論的 API
+  const { loading, error, data } = useFetch('http://localhost:1337/api/heroes') // 呼叫所有評論的 API
 
   if (loading) return <p>Loading....</p>
   if (error || !data) {
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 
 export default function Homepage() {
-  const { loading, error, data } = useFetch('https://grounded-compassion-7a840a9fe7.strapiapp.com/api/reviews') // 呼叫所有評論的 API
+  const { loading, error, data } = useFetch('http://localhost:1337/api/heroes') // 呼叫所有評論的 API
 
   if (loading) return <p>Loading....</p>
   if (error || !data) {
@@ -24,6 +24,7 @@ export default function Homepage() {
           <div className='rating'> {review.rating} </div>
           <h2>{review.title}</h2>
           <small> console list </small>
+          {/* <p> {review.body} </p> */}
           <p>{review.body[0]?.children[0]?.text.substring(0, 200)}...</p>
           {/* 正確使用 review.documentId 作為連結的參數 */}
           <Link to={`/details/${review.documentId}`}>Read more</Link>
